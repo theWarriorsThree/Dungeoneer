0019c4f9de1ff477f574c2ca37289d94783d5429
Liam <liam.d.kerr@gmail.com>
2014-11-25 23:44:07 +0000

    model changes

--- charactermanager/models.py
+++ charactermanager/models.py
@@ -9,84 +9,88 @@ class Character(models.Model):
 		return self.name
 
 class Ability(models.Model):
-	RECHARGE_CHOICES = (
-		('ATWILL', 'At-Will'),
-		('ENCOUNTER', 'Encounter'),
-		('DAILY', 'Daily')
-	)
-	ACTION_TYPE_CHOICES = (
-		('EMPTY','--'),
-		('STANDARD','Standard action'),
-		('MOVE','Move action'),
-		('INTERRUPT','Immediate Interrupt'),
-		('REACTION','Immediate reaction'),
-		('MINOR','Minor Action'),
-		('OPPORTUNTIY','Opportunity Action'),
-		('FREE','Free Action'),
-		('NONE','No Action')
-	)
 
-	ATTACK_TYPE_CHOICES = (
-		('EMPTY','--'),
-		('ME','Melee'),
-		('MB','Melee Base'),
-		('RA','Ranged'),
-		('RB','Ranged Basic'),
-		('MR','Melee or Ranged'),
-		('CB','Close Burst'),
-		('BL','Close Blast'),
-		('AB','Area Burst'),
-		('AW','Area Wall'),
-		('AA','Area'),
-		('PE','Personal')
-	)
+    RECHARGE_CHOICES = (
+        ('ATWILL', 'At-Will'),
+        ('ENCOUNTER', 'Encounter'),
+        ('DAILY', 'Daily')
+    )
+    ACTION_TYPE_CHOICES = (
+        ('EMPTY','--'),
+        ('STANDARD','Standard action'),
+        ('MOVE','Move action'),
+        ('INTERRUPT','Immediate Interrupt'),
+        ('REACTION','Immediate reaction'),
+        ('MINOR','Minor Action'),
+        ('OPPORTUNTIY','Opportunity Action'),
+        ('FREE','Free Action'),
+        ('NONE','No Action')
+    )
 
-	ATTACK_STAT_CHOICES = (
-		('EMPTY','--'),
-		('STENGTH','Strength'),
-		('CONSTITUTION','Constitution'),
-		('DEXTERITY','Dexterity'),
-		('INTELLIGENCE','Intelligence'),
-		('CHARISMA','Charisma'),
-		('WISDOM','Wisdom')
-	)
+    ATTACK_TYPE_CHOICES = (
+        ('EMPTY','--'),
+        ('ME','Melee'),
+        ('MB','Melee Base'),
+        ('RA','Ranged'),
+        ('RB','Ranged Basic'),
+        ('MR','Melee or Ranged'),
+        ('CB','Close Burst'),
+        ('BL','Close Blast'),
+        ('AB','Area Burst'),
+        ('AW','Area Wall'),
+        ('AA','Area'),
+        ('PE','Personal')
+    )
 
-	DEFENSE_STAT_CHOICES = (
-		('EMPTY','--'),
-		('AC','AC'),
-		('FORTITUDE','Fortitude'),
-		('REFLEX','Reflex'),
-		('WILL','Will')
-	)
+    ATTACK_STAT_CHOICES = (
+        ('EMPTY','--'),
+        ('STENGTH','Strength'),
+        ('CONSTITUTION','Constitution'),
+        ('DEXTERITY','Dexterity'),
+        ('INTELLIGENCE','Intelligence'),
+        ('CHARISMA','Charisma'),
+        ('WISDOM','Wisdom')
+    )
 
-	SUSTAIN_CHOICES = (
-		('EMPTY','--'),
-		('MINOR','Minor'),
-		('MOVE','Move'),
-		('STANDARD','Standard')
-	)
+    DEFENSE_STAT_CHOICES = (
+        ('EMPTY','--'),
+        ('AC','AC'),
+        ('FORTITUDE','Fortitude'),
+        ('REFLEX','Reflex'),
+        ('WILL','Will')
+    )
 
-	name = models.CharField(max_length=32)
-	level = models.CharField(max_length=32)
-	flavor = models.TextField()
-	recharge = models.CharField(max_length=2,choices=RECHARGE_CHOICES, default='ATWILL')
-	keywords = models.CharField(max_length=32)
-	actionType = models.CharField(max_length=2,choices=ACTION_TYPE_CHOICES, default='EMPTY')
-	attackType = models.CharField(max_length=2,choices=ATTACK_TYPE_CHOICES, default='EMPTY')
-	attackRange = models.CharField(max_length=32)
-	trigger = models.CharField(max_length=32)
-	prerequisite = models.CharField(max_length=32)
-	requirement = models.CharField(max_length=32)
-	target = models.CharField(max_length=32)
-	attackStat = models.CharField(max_length=2,choices=ATTACK_STAT_CHOICES, default='EMPTY')
-	attackStatNote = models.CharField(max_length=32)
-	defenseStat = models.CharField(max_length=2,choices=DEFENSE_STAT_CHOICES, default='EMPTY')
-	defenseStatNote = models.CharField(max_length=32)
-	hit = models.TextField()
-	miss = models.TextField()
-	secondaryTarget = models.CharField(max_length=32)
-	secondaryAttack = models.CharField(max_length=32)
-	effect = models.TextField()
-	sustainType = models.CharField(max_length=2,choices=SUSTAIN_CHOICES, default='EMPTY')
-	sustainNote = models.CharField(max_length=32)
-	special = models.TextField()
+    SUSTAIN_CHOICES = (
+        ('EMPTY','--'),
+        ('MINOR','Minor'),
+        ('MOVE','Move'),
+        ('STANDARD','Standard')
+    )
+
+    name = models.CharField(max_length=32,blank=True)
+    level = models.CharField(max_length=32,blank=True)
+    flavor = models.TextField(blank=True)
+    recharge = models.CharField(max_length=32,choices=RECHARGE_CHOICES, default='ATWILL',blank=True)
+    keywords = models.CharField(max_length=32,blank=True)
+    actionType = models.CharField(max_length=32,choices=ACTION_TYPE_CHOICES, default='EMPTY',blank=True)
+    attackType = models.CharField(max_length=32,choices=ATTACK_TYPE_CHOICES, default='EMPTY',blank=True)
+    attackRange = models.CharField(max_length=32,blank=True)
+    trigger = models.TextField(blank=True)
+    prerequisite = models.TextField(blank=True)
+    requirement = models.TextField(blank=True)
+    target = models.CharField(max_length=32,blank=True)
+    attackStat = models.CharField(max_length=32,choices=ATTACK_STAT_CHOICES, default='EMPTY',blank=True)
+    attackStatNote = models.CharField(max_length=32,blank=True)
+    defenseStat = models.CharField(max_length=32,choices=DEFENSE_STAT_CHOICES, default='EMPTY',blank=True)
+    defenseStatNote = models.CharField(max_length=32,blank=True)
+    hit = models.TextField(blank=True)
+    miss = models.TextField(blank=True)
+    secondaryTarget = models.CharField(max_length=32,blank=True)
+    secondaryAttack = models.CharField(max_length=32,blank=True)
+    effect = models.TextField(blank=True)
+    sustainType = models.CharField(max_length=32,choices=SUSTAIN_CHOICES, default='EMPTY',blank=True)
+    sustainNote = models.CharField(max_length=32,blank=True)
+    special = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.name
