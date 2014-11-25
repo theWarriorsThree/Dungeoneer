53cba35c69b155bd3ae31269f2d6c90d1f215af3
Liam <liam.d.kerr@gmail.com>
2014-11-25 22:55:53 +0000

    other updates

--- charactermanager/views.py
+++ charactermanager/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render_to_response, get_object_or_404, redirect
+from django.template import RequestContext
+from charactermanager.models import Ability
 # Create your views here.
+
+def abilities(request):
+	context = RequestContext(request)
+	context['abilites'] = Ability.objects.all().order_by('priority')
+	return render_to_response('dungeoneer/abilitiesTemplate.html', context)
